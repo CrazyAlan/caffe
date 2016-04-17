14a8de22f6cf88c15fa6e77765c9d340fb66a106
CrazyAlan <xiaochuan.chen91@gmail.com>
2016-04-17 12:59:29 -0700

    Not finished classification trial

--- python/pyloss.py
+++ python/pyloss.py
@@ -63,6 +63,42 @@ class AngularLossLayer(caffe.Layer):
             else:
                 bottom[i].diff[...] = deMultiangle2(bottom[1].data, bottom[0].data)/bottom[0].num/1.
             
+class AngularAccLayer(caffe.Layer):
+
+    def setup(self, bottom, top):
+        # check input pair
+        if len(bottom) != 6:
+            raise Exception("Need six inputs to compute distance.")
+
+    def reshape(self, bottom, top):
+        # check input dimensions match
+        if bottom[0].count != bottom[1].count:
+            raise Exception("Inputs must have the same dimension.")
+        # difference is shape of inputs
+        #self.diff = np.zeros_like(bottom[0].data, dtype=np.float32)
+        # loss output is scalar
+        top[0].reshape(1)
+
+    def forward(self, bottom, top):
+        #self.diff[...] = bottom[0].data - bottom[1].data
+        #print bottom[0].num
+        a0 = bottom[0].data
+        a1 = bottom[1].data
+        a2 = bottom[2].data
+
+        g0 = bottom[3].data
+        g1 = bottom[4].data
+        g2 = bottom[5].data
+
+        approx = (np.array([a0, a1, a2]).transpose())
+        actual = (np.array([g0, g1, g2]).transpose()) 
+        #print bottom[0].num
+        print approx
+
+        #top[0].data[...] = np.sum(multiangle(approx, actual)) / bottom[0].num/1.
+
+    def backward(self, top, propagate_down, bottom):
+        pass
         # for i in range(2):
         #     if not propagate_down[i]:
         #         continue
